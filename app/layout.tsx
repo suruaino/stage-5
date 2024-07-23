ab219ee925e4c5b66a958b032d5e00993ecab9b0
SURU EMMANUEL <suruaino@gmail.com>
2024-07-23 19:18:49 +0100

    worked on login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Instrument_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+  
+const instrumentSans = Instrument_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-instrument-sans', // Optional: to use as a CSS variable
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${instrumentSans.variable}`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      // backgroundColor: {
+      //   'default-btn-bg': '#633CFF', // Replace with your desired color code
+      // },
+
     },
   },
   plugins: [],
